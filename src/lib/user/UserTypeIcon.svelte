20b170393ea597eb807a123198d7f3468bf95cc3
IRHM <37304121+IRHM@users.noreply.github.com>
2025-01-01 18:53:31 +0000

    Create UserTypeIcon component, use in /users/ table

---
+++ src/lib/user/UserTypeIcon.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { UserType } from "@/types";
+  import Icon from "../Icon.svelte";
+
+  export let type: UserType;
+</script>
+
+{#if !type}
+  <span
+    style="font-family: 'Rampart One'; font-weight: bold; font-size: 21px; line-height: 20px; user-select: none;"
+  >
+    W
+  </span>
+{:else if type == UserType.Jellyfin}
+  <Icon i="jellyfin" wh={20} />
+{:else if type == UserType.Plex}
+  <Icon i="plex" wh={20} />
+{:else if type == UserType.Proxy}
+  <Icon i="lock-closed" wh={20} />
+{:else}
+  <!-- Unknown type -->
+  <span
+    style="font-family: 'Rampart One'; font-weight: bold; font-size: 21px; line-height: 20px; user-select: none;"
+  >
+    ?
+  </span>
+{/if}
